d2b6c489f3771a4abf4dcf37aa3c1d06459dcebe
Jyeager44 <yeagerj@student.swosu.edu>
2021-06-14 17:53:39 -0500

    pushing the scripts
    Credit 
    Mr. Kaiser from YouTube

---
+++ Scripts/BattleSystem.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST}
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject rougePrefab;
+    public GameObject magePrefab;
+    public GameObject knightPrefab;
+    public GameObject barbarianPrefab;
+    public GameObject EnemyPrefab;
+    public Transform rougeBattlePosistion;
+    public Transform mageBattlePosistion;
+    public Transform knightBattlePosistion;
+    public Transform barbarianBattlePosistion;
+    public Transform enemyBattlePosistion;
+
+    Unit RougeUnit;
+    //unit MageUnit;
+    //unit KnightUnit;
+    //unit BarbarianUnit;
+    Unit enemyUnit;
+    Unit playerUnit;
+
+    public BattleHUD rougeHUD;
+    public BattleHUD mageHUD;
+    public BattleHUD knightHUD;
+    public BattleHUD barbarianHUD;
+
+    public BattleState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        //SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        //GameObject RougeGO = Instantiate(rougePrefab, rougeBattlePosistion);
+        //GameObject MageGO = Instantiate(magePrefab, mageBattlePosistion);
+        //GameObject KnightGO = Instantiate(knightPrefab, knightBattlePosistion);
+        //GameObject BarbarianGO = Instantiate(barbarianPrefab, barbarianBattlePosistion);
+        //RougeGO.Getcomponent<Unit>();
+        //MageGO.Getcomponent<Unit>();
+        //KnightGO.Getcomponent<Unit>();
+        //BarbarianGO.Getcomponent<Unit>();
+        //GameObject EnemyGO =Instantiate(EnemyPrefab, enemyBattlePosistion);
+        //EnemyGO.Getcomponent<Unit>();
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        playerUnit = RougeUnit;
+        enemyUnit.currHP -= RougeUnit.attack;
+        bool isDead = enemyUnit.currHP= 0b0;
+        yield return new WaitForSeconds(2f);
+        if(isDead)
+        {
+            state = BattleState.WON;
+        }else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+       
+    }
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    void EnemyTurn()
+    {
+
+    }
+    void PlayerTurn()
+    {
+
+    }
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+        StartCoroutine(PlayerAttack());
+    }
+}
---
+++ Scripts/unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+    public int attack;
+    public int defence;
+    public int speed;
+    public int maxHP;
+    public int currHP;
+
+
+    public bool TakeDamage(int attack, int defence)
+    {
+
+        if (currHP <= 0)
+            return true;
+        else
+            return false;
+    }
+}
